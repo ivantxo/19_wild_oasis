dc9892c069fc77a259792e704077d208e98b95b4
IvanR <ivan.ramirezf@gmail.com>
2025-04-14 23:09:01 +1000

    Configure esLint II
    - Configure esLint:
    npm install --save-dev eslint @eslint/js
    
    - Create JavaScript configuration file
    touch eslint.config.js
    
    - Add code in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
 
-export default [
-  { ignores: ['dist'] },
+export default defineConfig([
   {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
+    files: ["**/*.js"],
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      js,
     },
+    extends: ["js/recommended"],
     rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
     },
   },
-]
+]);
