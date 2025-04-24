d476ea21f4160e7063161de7d2685543a632b829
IvanR <ivan.ramirezf@gmail.com>
2025-04-24 21:39:51 +1000

    AppLayout - Started styling main app

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,15 +1,22 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+
+const Main = styled.main`
+  background-color: green;
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 function AppLayout() {
   return (
     <div>
       <Header />
       <Sidebar />
-      <main>
+      <Main>
         <Outlet />
-      </main>
+      </Main>
     </div>
   );
 }
