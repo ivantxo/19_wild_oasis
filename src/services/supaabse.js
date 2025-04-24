addfb8c2b1230eac2a1a68e523a4c807ae56e693
IvanR <ivan.ramirezf@gmail.com>
2025-04-25 01:11:53 +1000

    Supabase II - Connect Supabase with React
    - Created a Supabase service. This handles the connection to Supabase
    - Created a Cabins service, this handles for now retrieving the data of
    the cabins table

---
+++ src/services/supaabse.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://vfwzmmooxyjpqhpyfrvg.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmd3ptbW9veHlqcHFocHlmcnZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MDEzNjUsImV4cCI6MjA2MTA3NzM2NX0.3soZaWY_HuXQvNkBpJwKvaRMWJrDRvAKG7qFZnpkxZg";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
