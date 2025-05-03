8d2bea4c7effc6eb50787ee549c99afe81c611f2
IvanR <ivan.ramirezf@gmail.com>
2025-05-03 21:41:51 +1000

    Secrets added to .env file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://vfwzmmooxyjpqhpyfrvg.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmd3ptbW9veHlqcHFocHlmcnZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MDEzNjUsImV4cCI6MjA2MTA3NzM2NX0.3soZaWY_HuXQvNkBpJwKvaRMWJrDRvAKG7qFZnpkxZg";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
