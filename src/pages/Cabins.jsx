488d7ae0f327d378ce8ddcfccf01b206edd90087
IvanR <ivan.ramirezf@gmail.com>
2025-05-12 22:24:01 +1000

    Added Modal component
    - New Modal component, click on Add new cabin, modal window

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,12 +3,9 @@ import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
-import Button from "../ui/Button";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
-
   return (
     <>
       <Row type="horizontal">
@@ -20,10 +17,9 @@ function Cabins() {
         <CabinTable />
       </Row>
 
-      <Button variation="primary" onClick={() => setShowForm((show) => !show)}>
-        Add new cabin
-      </Button>
-      {showForm && <CreateCabinForm />}
+      <Row>
+        <AddCabin />
+      </Row>
     </>
   );
 }
