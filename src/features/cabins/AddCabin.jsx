90bf41f1de9656fac89c4f0949682bc749968698
IvanR <ivan.ramirezf@gmail.com>
2025-05-14 22:49:53 +1000

    Compound Component
    - Modal compound component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,26 +1,29 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
 
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button
-        size="medium"
-        variation="primary"
-        onClick={() => setIsOpenModal((show) => !show)}
-      >
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button variation="primary" size="medium">
+          Add new cabin
+        </Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button variation="primary" size="medium">
+          Show table
+        </Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }
 
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,5 +1,8 @@
+import { cloneElement, createContext, useContext, useState } from "react";
+import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import Button from "./Button";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -25,42 +28,71 @@ const Overlay = styled.div`
   transition: all 0.5s;
 `;
 
-const Button = styled.button`
-  background: none;
-  border: none;
-  padding: 0.4rem;
-  border-radius: var(--border-radius-sm);
-  transform: translateX(0.8rem);
-  transition: all 0.2s;
-  position: absolute;
-  top: 1.2rem;
-  right: 1.9rem;
+// const Button = styled.button`
+//   background: none;
+//   border: none;
+//   padding: 0.4rem;
+//   border-radius: var(--border-radius-sm);
+//   transform: translateX(0.8rem);
+//   transition: all 0.2s;
+//   position: absolute;
+//   top: 1.2rem;
+//   right: 1.9rem;
 
-  &:hover {
-    background-color: var(--color-grey-100);
-  }
+//   &:hover {
+//     background-color: var(--color-grey-100);
+//   }
 
-  & svg {
-    width: 2.4rem;
-    height: 2.4rem;
-    /* Sometimes we need both */
-    /* fill: var(--color-grey-500);
-    stroke: var(--color-grey-500); */
-    color: var(--color-grey-500);
-  }
-`;
+//   & svg {
+//     width: 2.4rem;
+//     height: 2.4rem;
+//     /* Sometimes we need both */
+//     /* fill: var(--color-grey-500);
+//     stroke: var(--color-grey-500); */
+//     color: var(--color-grey-500);
+//   }
+// `;
 
-function Modal({ children, onClose }) {
+const ModalContext = createContext();
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState("");
+
+  const close = () => setOpenName("");
+  const open = setOpenName;
+
   return (
-    <Overlay>
-      <StyledModal>
-        <Button onClick={onClose}>
-          <HiXMark />
-        </Button>
-        <div>{children}</div>
-      </StyledModal>
-    </Overlay>
+    <ModalContext.Provider value={{ openName, close, open }}>
+      {children}
+    </ModalContext.Provider>
   );
 }
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+  if (name !== openName) return null;
+
+  return createPortal(
+    <Overlay>
+      <StyledModal>
+        <Button>
+          <HiXMark onClick={close} />
+        </Button>
+
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
+      </StyledModal>
+    </Overlay>,
+    document.body
+  );
+}
+
+Modal.Open = Open;
+Modal.Window = Window;
 
 export default Modal;
