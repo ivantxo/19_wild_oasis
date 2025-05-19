1a836796c865fae8c81a7ed6baaa38c3dc2c4e84
IvanR <ivan.ramirezf@gmail.com>
2025-05-20 00:32:48 +1000

    Filter III
    - Filtering cabins by param value from useSearchParams

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -30,6 +30,13 @@ function CabinTable() {
 
   const filterValue = searchParams.get("discount") || "all";
 
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -43,7 +50,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
