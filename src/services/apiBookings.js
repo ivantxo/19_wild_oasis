97f85e55b1ba1227bb4b54079d02bbcafaed7fd4
IvanR <ivan.ramirezf@gmail.com>
2025-05-27 00:01:16 +1000

    Pagination III
    - Pagination working in this commit

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,5 +1,6 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
+import { PAGE_SIZE } from "../utils/constants";
 
 export async function getBooking(id) {
   const { data, error } = await supabase
@@ -16,7 +17,7 @@ export async function getBooking(id) {
   return data;
 }
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
     .select(
@@ -32,6 +33,13 @@ export async function getBookings({ filter, sortBy }) {
     query = query.order(sortBy.field, {
       ascending: sortBy.direction === "asc",
     });
+
+  // PAGINATION
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
 
   const { data, error, count } = await query;
 
