8a602786c17b17d9967157b8a61b6858c5ac33c2
IvanR <ivan.ramirezf@gmail.com>
2025-05-31 00:33:35 +1000

    Checkout I
    - New custom hook useCheckout
    - Implemented checking out a customer

---
+++ src/features/check-in-out/useCheckout.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+import { updateBooking } from "../../services/apiBookings";
+
+export function useCheckout() {
+  const queryClient = useQueryClient();
+  const { mutate: checkout, isPending: isCheckinOut } = useMutation({
+    mutationFn: (bookingId) =>
+      updateBooking(bookingId, {
+        status: "checked-out",
+      }),
+
+    onSuccess: (data) => {
+      console.log("data", data);
+      toast.success(`Booking #${data.id} successfully checked out`);
+      queryClient.invalidateQueries({ active: true });
+    },
+
+    onError: () => toast.error("There was an error while checkin out"),
+  });
+
+  return { checkout, isCheckinOut };
+}
