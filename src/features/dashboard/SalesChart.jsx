7db0c3fd87f85944c9e713668d01d621c40fd4c3
IvanR <ivan.ramirezf@gmail.com>
2025-06-13 23:30:31 +1000

    Dashboard V
    - Added SalesChart component and display it in DashboardLayout

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import DashboardBox from "./DashboardBox";
+import Heading from "../../ui/Heading";
 
 const StyledSalesChart = styled(DashboardBox)`
   grid-column: 1 / -1;
@@ -57,3 +58,13 @@ const colors = isDarkMode
       text: "#374151",
       background: "#fff",
     };
+
+function SalesChart() {
+  return (
+    <StyledSalesChart>
+      <Heading as="h1">Sales</Heading>
+    </StyledSalesChart>
+  );
+}
+
+export default SalesChart;
